83cca7d0e5999f4839fd48d0761ef542ed52eec8
felipemike <felipeclemente2012@gmail.com>
2022-12-13 21:33:45 -0300

    02

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 export default class Login extends Component {
+  state = {
+    name: '',
+    buttonDisabled: true,
+    loading: false,
+  };
+
+  handleClick = () => {
+    const { name } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true });
+    createUser({ name }).then(() => history.push('/search'));
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    const number = 3;
+    this.setState({
+      [name]: value,
+    });
+    if (name === 'name') {
+      this.setState({
+        buttonDisabled: value.length < number,
+      });
+    }
+  };
+
   render() {
+    const { name, loading, buttonDisabled } = this.state;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login" className="page-login">
+        {loading ? (
+          <Loading loading={ loading } />
+        ) : (
+          <form>
+            <label htmlFor="login-name-input">
+              Nome:
+              <input
+                id="login-name-input"
+                data-testid="login-name-input"
+                name="name"
+                type="text"
+                value={ name }
+                onChange={ this.handleChange }
+              />
+            </label>
+            ;
+            <button
+              type="submit"
+              data-testid="login-submit-button"
+              onClick={ this.handleClick }
+              disabled={ buttonDisabled }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
